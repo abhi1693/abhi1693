e6a3af8d92a9b54731b6f1de99f50e1335a6f608
Abhimanyu Saharan <asaharan@onemindservices.com>
2025-06-07 16:06:36 +0530

    Update README file extension from .md to .adoc in ci.yml and main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ def main() -> None:
     environment = Environment(loader=FileSystemLoader('templates/'))
     template: Template = environment.get_template('template.adoc')
     document: str = template.render(bio=BIO, posts=fetch_posts(), video=fetch_video(), updated=datetime.today())
-    with open('./README.md', 'w') as readme:
+    with open('./README.adoc', 'w') as readme:
         readme.write(document)
 
 
